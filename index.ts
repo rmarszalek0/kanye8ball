c9953608b01371fd62318b1438080237900cd0cd
rmarszalek0 <111804063+rmarszalek0@users.noreply.github.com>
2022-11-28 16:57:52 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,9 @@
-import kupa from "./exporter.js";
 
 const button = document.querySelector('.send')
 const answer = document.querySelector('.answer-container')
-
-console.log(kupa)
     
 button?.addEventListener
     ('click', getKanyeQuote)
-
 
 async function getKanyeQuote() {
     const promise = await fetch('https://api.kanye.rest');
